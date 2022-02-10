8b8a59b91bdca1fb922a10a3690937105da270be
borgesgfj <borgesgfj@gmail.com>
2022-02-10 16:26:06 -0300

    fea: create ORM module
    - create docke- compose file
    
    - crete TypeORM module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { OrmModule } from 'global_modules/typeORM.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { TransfersModule } from './transfers/transfers.module';
 import { HistoryModule } from './user-history/user-history.module';
 
 @Module({
-  imports: [AccountsModule, TransfersModule, HistoryModule],
+  imports: [AccountsModule, TransfersModule, HistoryModule, OrmModule],
 })
 export class AppModule {}
